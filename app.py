90c1bfb19248e1e2076d8c9ec36979760fef8474
michrome <m+nitrous@j4e.name>
2016-06-05 10:30:05 +0000

    Form, passing variable to response

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/money', methods=['post'])
+def money():
+    amount = request.form['amount']
+    return render_template('money.html', amount=amount)
 
 if __name__ == '__main__':
     app.run()
