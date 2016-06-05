8e1f12195f500bfc9d25df0b0c74df958225003f
michrome <m+nitrous@j4e.name>
2016-06-05 22:33:02 +0000

    Refactor: move denominations into class and out of cash method

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ from collections import OrderedDict
 app = Flask(__name__)
 
 class Money:
+  denominations = OrderedDict()
+  denominations['50 pound notes'] = 5000
+  denominations['20 pound notes'] = 2000
+  denominations['10 pound notes'] = 1000
+  denominations['5 pound notes'] = 500
+  denominations['2 pound coins'] = 200
+  denominations['1 pound coins'] = 100
+  denominations['50p coins'] = 50
+  denominations['20p coins'] = 20
+  denominations['10p coins'] = 10
+  denominations['5p coins'] = 5
+  denominations['2p coins'] = 2
+  denominations['1p coins'] = 1
+
   def __init__(self, amount):
     self.amount=int(Decimal(amount)*100)
 
@@ -11,26 +25,13 @@ class Money:
     return str(self.amount/100.00)
 
   def cash(self):
-    denominations = OrderedDict()
-    denominations['50 pound notes'] = 5000
-    denominations['20 pound notes'] = 2000
-    denominations['10 pound notes'] = 1000
-    denominations['5 pound notes'] = 500
-    denominations['2 pound coins'] = 200
-    denominations['1 pound coins'] = 100
-    denominations['50p coins'] = 50
-    denominations['20p coins'] = 20
-    denominations['10p coins'] = 10
-    denominations['5p coins'] = 5
-    denominations['2p coins'] = 2
-    denominations['1p coins'] = 1
     cash = []
     remaining_total = self.amount
-    for key in denominations:
-      number_required = int(remaining_total/denominations[key])
+    for key in self.denominations:
+      number_required = int(remaining_total/self.denominations[key])
       cash.append("{denomination}: {number_required}"
                   .format(denomination=key, number_required=number_required))
-      remaining_total -= (denominations[key]*number_required)
+      remaining_total -= (self.denominations[key]*number_required)
     return cash
 
 @app.route('/')
