2c43160a54d245638f5d21cfc9f5ea35d47f23cc
michrome <m+nitrous@j4e.name>
2016-06-05 22:26:34 +0000

    Output the cash required for the amount of money

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from decimal import Decimal
+from collections import OrderedDict
 app = Flask(__name__)
 
 class Money:
@@ -8,6 +9,29 @@ class Money:
 
   def __repr__(self):
     return str(self.amount/100.00)
+
+  def cash(self):
+    denominations = OrderedDict()
+    denominations['50 pound notes'] = 5000
+    denominations['20 pound notes'] = 2000
+    denominations['10 pound notes'] = 1000
+    denominations['5 pound notes'] = 500
+    denominations['2 pound coins'] = 200
+    denominations['1 pound coins'] = 100
+    denominations['50p coins'] = 50
+    denominations['20p coins'] = 20
+    denominations['10p coins'] = 10
+    denominations['5p coins'] = 5
+    denominations['2p coins'] = 2
+    denominations['1p coins'] = 1
+    cash = []
+    remaining_total = self.amount
+    for key in denominations:
+      number_required = int(remaining_total/denominations[key])
+      cash.append("{denomination}: {number_required}"
+                  .format(denomination=key, number_required=number_required))
+      remaining_total -= (denominations[key]*number_required)
+    return cash
 
 @app.route('/')
 def index():
